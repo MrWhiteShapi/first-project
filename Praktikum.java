e153980bbc9258319653c90afef96b32d671543f
MrWhiteShapi <aigunovshapi@yandex.ru>
2023-09-13 16:28:31 +0300

    Это просто еще один коммит по курсу Yandex.Praktikum

--- Praktikum.java
+++ Praktikum.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.Scanner;
 public class Praktikum {
     public static void main(String[] args) {
+        //Этот комментарий нужен для того чтобы снова закомитить файл
         Scanner scanner = new Scanner(System.in);
         System.out.println("Сколько денег у вас осталось до зарплаты?");
         double moneyBeforeSalary = scanner.nextDouble();
